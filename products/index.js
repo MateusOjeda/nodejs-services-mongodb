e24a7f78d307d7f0a179176c7f1fe39a4c2694f8
Mateus Ojeda <mateus.ojeda.alv@gmail.com>
2022-09-17 17:35:44 -0300

    first commit - running services with mongo connect

---
+++ products/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());    
+
+app.use('/', (req,res,next) => {
+    return res.status(200).json({"msg": "Hello from Product"})
+})
+
+app.listen(8002,  () => {
+    console.log('Product is listening port 8002');
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+# NodeJs microsservices project:
+
+credits: Code with Jay - https://www.youtube.com/watch?v=EXDkgjU8DDU
+
+![Alt text](docs/architecture.jpg?raw=true "Scheme")
+
